18a6eefab8c45cc1014eb9609a508ed5d3dc706c
vik <theheero007@Gmail.com>
2025-10-06 00:54:14 +0530

    Time: 0 ms (100.00%) | Memory: 32.6 MB (25.00%) - LeetSync

---
+++ 4058-compute-alternating-sum/compute-alternating-sum.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int alternatingSum(vector<int>& nums) {
+        int sum = 0;
+        for(int i=0;i<nums.size();i++)
+        {
+            sum += (i % 2 == 0 ? nums[i] : -nums[i]);
+        }
+        return sum;
+    }
+};
