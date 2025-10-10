cd30b712219bcdd828d34e95f2586a3a5f9d05ed
vik <theheero007@Gmail.com>
2025-10-10 15:00:16 +0530

    Time: 97 ms (29.13%) | Memory: 55.7 MB (10.51%) - LeetSync

---
+++ 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool check(string s,unordered_map<char,int> hash)
+    {
+        for(auto it : s)
+        {
+            if(hash[it] > 0) 
+            {
+                hash[it]--;
+                continue;
+            }
+            if(hash[it] <= 0) return false;
+        }
+        return true;
+    }
+    int countCharacters(vector<string>& words, string chars) {
+        unordered_map<char,int> hash;
+        int count = 0;
+
+        for(char st : chars)
+        {
+            hash[st]++;
+        }
+        for(auto it : words)
+        {
+           if (check(it,hash))
+           {
+            count += it.size();
+           }
+        }
+        return count;
+    }
+};
