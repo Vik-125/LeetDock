3e5b454f6550da38b019c33a3aee40e2b71b426e
vik <theheero007@Gmail.com>
2025-10-21 11:25:21 +0530

    Time: 1 ms (34.57%) | Memory: 25.9 MB (69.14%) - LeetSync

---
+++ 3164-last-visited-integers/last-visited-integers.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> lastVisitedIntegers(vector<int>& nums) {
+        vector<int> post;
+        vector<int> ans;
+        int k = 0;
+
+        for(auto it : nums)
+        {
+            if(it > 0)
+            {
+                post.insert(post.begin(),it);
+                k = 0;
+            }
+            else if(it == -1)
+            {
+                k++;
+                if(k <= post.size())
+                {
+                    ans.push_back(post[k - 1]);
+                }
+                else if(k > post.size())
+                {
+                    ans.push_back(-1);
+                }
+            }
+        }
+        return ans;
+    }
+};
