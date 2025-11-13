dfff78c02f586cfd0f7a508f4e18f160f7c43a5b
vik <theheero007@Gmail.com>
2025-11-13 18:49:48 +0530

    Time: 38 ms (57.43%) | Memory: 74.3 MB (61.43%) - LeetSync

---
+++ 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    void dfs(int i,int j,int R,int C,vector<vector<int>> &land,vector<vector<bool>>& vis,int &er,int &ec,int drow[],int dcol[])
+    {
+        vis[i][j] =  true;
+        er = max(i,er);
+        ec = max(j,ec);
+        for(int k=0;k<4;k++)
+        {
+            int nr = i + drow[k];
+            int nc = j + dcol[k];
+            if(nr >= 0 && nc >= 0 && nr < R && nc < C && !vis[nr][nc] && land[nr][nc] == 1)
+            {
+                dfs(nr,nc,R,C,land,vis,er,ec,drow,dcol);
+            }
+        }
+        return;
+    }
+    vector<vector<int>> findFarmland(vector<vector<int>>& land) {
+        int n = land.size();
+        int m = land[0].size();
+        vector<vector<bool>> vis (n,vector<bool>(m,false));
+        vector<vector<int>> ans;
+
+        int drow[] = {-1,0,1,0};
+        int dcol[] = {0,1,0,-1};
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(!vis[i][j] && land[i][j] == 1)
+                {
+                    int er = 0;
+                    int ec = 0;
+                    dfs(i,j,n,m,land,vis,er,ec,drow,dcol);
+                    ans.push_back({i,j,er,ec});
+                }
+            }
+        }
+        return ans;
+    }
+};
