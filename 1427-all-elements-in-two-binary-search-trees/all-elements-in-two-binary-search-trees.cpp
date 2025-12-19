4dcb211bdddae02957dd42a83fde4ea299908341
vik <theheero007@Gmail.com>
2025-12-19 20:00:02 +0530

    Time: 2 ms (85.76%) | Memory: 88.5 MB (29.28%) - LeetSync

--- 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.cpp
+++ 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.cpp
@@ -11,20 +11,34 @@
  */
 class Solution {
 public:
-    void in(TreeNode* r,vector<int> &res)
+    void trav(TreeNode* r,vector<int> &res)
     {
         if(!r) return;
+        trav(r -> left,res);
         res.push_back(r -> val);
-        in(r -> left,res);
-        in(r -> right,res);
+        trav(r -> right,res);
         return;
     }
+    vector<int> merge(vector<int> &res1,vector<int> &res2)
+    {
+        vector<int> merged;
+        int i=0,j=0;
+        if(res1.size() == 0 && res2.size() == 0) return {};
+        while(i < res1.size() && j < res2.size())
+        {
+            if(res1[i] < res2[j]) merged.push_back(res1[i++]);
+            else merged.push_back(res2[j++]);
+        }
+        while(i < res1.size()) merged.push_back(res1[i++]);
+        while(j < res2.size()) merged.push_back(res2[j++]);
+        return merged;
+    }
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
-        vector<int> res;
-        in(root1,res);
-        in(root2,res);
+        vector<int> res1;
+        vector<int> res2;
+        trav(root1,res1);
+        trav(root2,res2);
 
-        sort(res.begin(),res.end());
-        return res;
+        return merge(res1,res2);
     }
 };
