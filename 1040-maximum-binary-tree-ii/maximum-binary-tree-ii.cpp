d3f49279e71a286f27f645c3216e99b12bcf26e0
vik <theheero007@Gmail.com>
2025-12-22 13:09:44 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (57.51%) - LeetSync

---
+++ 1040-maximum-binary-tree-ii/maximum-binary-tree-ii.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoMaxTree(TreeNode* root, int val) {
+        if(!root) return new TreeNode(val);
+        if(val > root -> val)
+        {
+            TreeNode* temp = new TreeNode(val);
+            temp -> left = root;
+            return temp;
+        }
+        else 
+        {
+            root -> right = insertIntoMaxTree(root -> right,val);
+        }
+        return root;
+    }
+};
