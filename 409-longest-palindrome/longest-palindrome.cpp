5cadfee1367559205e29f13c29cb30e9e622ca16
vik <theheero007@Gmail.com>
2026-02-28 22:17:11 +0530

    Time: 3 ms (16.79%) | Memory: 9.2 MB (61.63%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        int n = s.size();
+
+        map<char,int> hash;
+        for(auto it : s)
+        {
+            hash[it]++;
+        }
+
+        int ans = 0;
+        for(auto i : hash)
+        {
+            if(i.second % 2 == 0) ans += i.second;
+            else if(i.second % 2 != 0 && i.second > 1) ans += (i.second-1);
+        }
+        cout << ans;
+        return ans == n ? ans : ans + 1;
+    }
+};
