898d0a360036ec866cd59999019c092571f45344
vik <theheero007@Gmail.com>
2026-03-26 22:24:33 +0530

    Time: 4 ms (53.25%) | Memory: 97.3 MB (18.68%) - LeetSync

--- 2836-neither-minimum-nor-maximum/neither-minimum-nor-maximum.cpp
+++ 2836-neither-minimum-nor-maximum/neither-minimum-nor-maximum.cpp
@@ -2,7 +2,11 @@ class Solution {
 public:
     int findNonMinOrMax(vector<int>& nums) {
         if(nums.size() <= 2) return -1;
-        sort(nums.begin(),nums.end());
-        return nums[1];
+        int maxi = *max_element(nums.begin(),nums.end());
+        int mini = *min_element(nums.begin(),nums.end());
+        for(int i=0;i<3;i++){
+            if(mini != nums[i] && maxi != nums[i]) return nums[i];
+        }
+        return 0;
     }
 };
