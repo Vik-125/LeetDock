0cb317d8739e95515f219b4fa08553db76eddc94
vik <theheero007@Gmail.com>
2026-04-01 17:46:55 +0530

    Time: 0 ms (100.00%) | Memory: 23.6 MB (64.52%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -2,15 +2,38 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int,int> hash;
-        vector<int> ans;
+        
+        int ele1=0,ele2=0;
+        int c1=0,c2=0;
 
         for(auto it : nums){
-            hash[it]++;
+            if(c1 == 0 && ele2 != it){
+                ele1 = it;
+                c1++;
+            }
+            else if(c2 == 0 && ele1 != it){
+                ele2 = it;
+                c2++;
+            }
+
+            else if(ele1 == it) c1++;
+            else if(ele2 == it) c2++;
+
+            else {
+                c1--;
+                c2--;
+            }
         }
-        for(auto it : hash){
-            if(it.second > n/3) ans.push_back(it.first);
+        c1 = 0;
+        c2 = 0;
+        cout << ele1 << " " << ele2;
+        for(auto it : nums){
+            if(it == ele1) c1++;
+            else if(it == ele2) c2++;
         }
-        return ans;
+        if(c1 > (n/3) && c2 > (n/3)) return {ele1,ele2};
+        if(c1 > (n/3)) return{ele1};
+        if(c2 > (n/3)) return{ele2};
+        else return {};
     }
 };
