ecc01455a6d7d06716a6224fbeac9c70d77ef86f
vik <theheero007@Gmail.com>
2026-04-03 17:16:41 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (40.92%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,14 +1,29 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int k = 0;
-        for(int i=m;i<nums1.size();i++)
-        {
-            nums1[i] = nums2[k];
-            k++;
-        }
+        int i=0;
+        int j=0;
+        vector<int> mergedArray;
 
-        sort(nums1.begin(),nums1.end());
+        while(i < m && j < n){
+            if(nums1[i] <= nums2[j]){
+                mergedArray.push_back(nums1[i]);
+                i++;
+            }
+            else{
+                mergedArray.push_back(nums2[j]);
+                j++;
+            }
+        }
+        while(i < m){
+            mergedArray.push_back(nums1[i]);
+            i++;
+        }
+        while(j < n){
+            mergedArray.push_back(nums2[j]);
+            j++;
+        }
+        nums1 = mergedArray;
         return;
     }
 };
