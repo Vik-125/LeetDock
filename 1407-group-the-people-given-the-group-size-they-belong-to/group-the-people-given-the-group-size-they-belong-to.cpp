fa45b63c13a1e2781ff807eab3d9b8b42b0c19f6
vik <theheero007@Gmail.com>
2026-04-20 23:02:54 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (59.59%) - LeetSync

--- 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.cpp
@@ -3,13 +3,14 @@ public:
     vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
         vector<vector<int>> ans;
         int n = groupSizes.size();
-        unordered_map<int, vector<int>> hash;;
-\
+        unordered_map<int, vector<int>> hash;
+
         for (int i = 0; i < n; i++) {
-            hash[groupSizes[i]].push_back(i);
-            if (hash[groupSizes[i]].size() == groupSizes[i]) {
-                ans.push_back(hash[groupSizes[i]]);
-                hash[groupSizes[i]].clear();
+            int k = groupSizes[i];
+            hash[k].push_back(i);
+            if (hash[k].size() == k) {
+                ans.push_back(hash[k]);
+                hash[k].clear();
             }
         }
         return ans;
