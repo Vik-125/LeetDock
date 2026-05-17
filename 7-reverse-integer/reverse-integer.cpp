4b63218277ef924ce0103d44fd3fd4c9c806680b
vik <theheero007@Gmail.com>
2026-05-17 21:21:59 +0530

    Time: 3 ms (29.45%) | Memory: 8.7 MB (19.63%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -2,20 +2,13 @@ class Solution {
 public:
     int reverse(int x) {
         long long rev = 0;
-        long long y = x;
-        int flag = 0;
-        if(x < 0){
-            y = y * -1;
-            flag = 1;
-        }
-        while(y > 0){
-            int dig = y%10;
+
+        while(x != 0){
+            int dig = x%10;
             rev = rev*10 + dig;
-            y /= 10;
+            x /= 10;
         }
-        long long lim = pow(2,31);
-        if(rev < -lim || rev > lim-1) return 0;
-        if(flag == 1) return 0-rev;
-        return rev;
+        if(rev < INT_MIN || rev > INT_MAX) return 0;
+        return (int)rev;
     }
 };
