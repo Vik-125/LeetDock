3a151b5640c5038496a816217bce47a60184bc27
vik <theheero007@Gmail.com>
2026-05-27 12:30:47 +0530

    Time: 137 ms (12.92%) | Memory: 25.7 MB (23.31%) - LeetSync

---
+++ 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+        map<int,int> hash;
+        int cnt = 0;
+        for(int i=0;i<word.size();i++){
+            int k = (int)word[i];
+            if(k >= 65 && k <= 91 && hash.count(k)){
+                continue;
+            }
+            hash[k] = i;
+        }
+        for(auto it : hash){
+            int a = it.first;
+            int b = it.second;
+            if(hash.count(a-32) && hash[a-32] > b) cnt++;
+        }
+        return cnt;
+    }
+};
