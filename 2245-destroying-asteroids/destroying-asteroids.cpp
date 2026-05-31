535654af3cbd843f354ede9d4eddf18e693b3ce1
vik <theheero007@Gmail.com>
2026-05-31 14:15:36 +0530

    Time: 23 ms (93.28%) | Memory: 106.8 MB (48.03%) - LeetSync

--- 2245-destroying-asteroids/destroying-asteroids.cpp
+++ 2245-destroying-asteroids/destroying-asteroids.cpp
@@ -2,14 +2,13 @@ class Solution {
 public:
     bool asteroidsDestroyed(long long mass, vector<int>& asteroids) {
         int n = asteroids.size();
-        bool flag = true;
         sort(asteroids.begin(),asteroids.end());
 
         for(auto it : asteroids){
-            if(mass > it || mass == it) mass += it;
-            else flag = false;
+            if(mass < it) return false;
+            else mass += it;
         }
         
-        return flag;
+        return true;
     }
 };
