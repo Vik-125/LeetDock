611af5ad31e91cff37dd125d3f13a0bd3c180cc0
vik <theheero007@Gmail.com>
2026-06-11 12:07:05 +0530

    Time: 26 ms (59.51%) | Memory: 22.1 MB (43.42%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -15,26 +15,48 @@ public:
     // }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,1e7));
-        
+        // tabulation
+        // vector<vector<int>> dp(n,vector<int>(amount+1,1e7));
+        // for(int i=0;i<n;i++) dp[i][0] = 0;
 
-        for(int i=0;i<n;i++) dp[i][0] = 0;
+        // for(int t=1;t<=amount;t++){
+        //     if(t % coins[0] == 0) dp[0][t] = t/coins[0];
+        //     else dp[0][t] = 1e7;
+        // }
+        // for(int i=1;i<n;i++){
+        //     for(int t=1;t<=amount;t++){
+        //         int notPick = dp[i-1][t];
+        //         int pick = 1e7;
+        //         if(t >= coins[i]){
+        //             pick = 1 + dp[i][t-coins[i]];
+        //         }
+        //         dp[i][t] = min(pick, notPick);
+        //     }
+        // }
+        // int noOfCoins = dp[n-1][amount];
 
-        for(int t=1;t<=amount;t++){
-            if(t % coins[0] == 0) dp[0][t] = t/coins[0];
-            else dp[0][t] = 1e7;
-        }
-        for(int i=1;i<n;i++){
-            for(int t=1;t<=amount;t++){
-                int notPick = dp[i-1][t];
-                int pick = 1e7;
-                if(t >= coins[i]){
-                    pick = 1 + dp[i][t-coins[i]];
-                }
-                dp[i][t] = min(pick, notPick);
+
+        // Space optimization
+        vector<int> cur(amount+1,1e7), prev(amount+1,1e7);
+
+        for(int j=0;j<=amount;j++){
+            if(j % coins[0] == 0){
+                prev[j] = j/coins[0];
             }
         }
-        int noOfCoins = dp[n-1][amount];
+        for(int i=1;i<n;i++){
+            for(int amt=0;amt<=amount;amt++){
+                int notPick = prev[amt];
+                int pick = 1e7;
+                if(coins[i] <= amt){
+                    pick = 1 +  cur[amt - coins[i]];
+                }
+
+                cur[amt] = min(pick,notPick);
+            }
+            prev = cur;
+        }
+        int noOfCoins = prev[amount];
         return noOfCoins == 1e7 ? -1 : noOfCoins;
     }
 };
