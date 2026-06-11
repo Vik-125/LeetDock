a0f956f4900d95558e12cbd13a27756633a94902
vik <theheero007@Gmail.com>
2026-06-11 14:01:13 +0530

    Time: 27 ms (64.75%) | Memory: 36.9 MB (33.68%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int findWays(int i, int amount, vector<int>& coins, vector<vector<int>> &dp){
-        if(i == coins.size() || amount < 0) return 0;
+        if(i < 0 || amount < 0) return 0;
         if(amount == 0) return 1;
         if(dp[i][amount] != -1) return dp[i][amount];
 
-        int notPick = findWays(i+1, amount, coins, dp);
+        int notPick = findWays(i-1, amount, coins, dp);
         int pick = 0;
         if(amount >= coins[i]){
             pick = findWays(i, amount-coins[i], coins, dp);
@@ -15,6 +15,6 @@ public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
         vector<vector<int>> dp(n, vector<int>(amount+1,-1));
-        return findWays(0, amount, coins, dp);
+        return findWays(n-1, amount, coins, dp);
     }
 };
