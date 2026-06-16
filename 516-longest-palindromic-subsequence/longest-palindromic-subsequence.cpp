80aa7ae997a3aae855abdb6cc30828d8ea31d600
vik <theheero007@Gmail.com>
2026-06-16 20:44:12 +0530

    Time: 46 ms (91.05%) | Memory: 9.5 MB (91.49%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -14,20 +14,41 @@ public:
         reverse(s.begin(),s.end());
         int n = s.size();
 
-        vector<vector<int>> dp(n+1, vector<int>(n+1,-1));
+        // Tabulation
+        // vector<vector<int>> dp(n+1, vector<int>(n+1,-1));
 
-        for(int i=0;i<=n;i++) dp[i][0] = 0;
-        for(int j=0;j<=n;j++) dp[0][j] = 0;
+        // for(int i=0;i<=n;i++) dp[i][0] = 0;
+        // for(int j=0;j<=n;j++) dp[0][j] = 0;
     
 
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=n;j++){
+        //         if(s[i-1] == t[j-1]) {
+        //             dp[i][j] = 1 + dp[i-1][j-1];
+        //             }
+        //         else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+        //     }
+        // }
+
+        vector<int> prev(n+1,0), cur(n+1,0) ;
+
+        for(int i=0;i<=n;i++) cur[0] = 0;
+    
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
                 if(s[i-1] == t[j-1]) {
-                    dp[i][j] = 1 + dp[i-1][j-1];
+                    cur[j] = 1 + prev[j-1];
                     }
-                else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                else cur[j] = max(prev[j],cur[j-1]);
             }
+            prev = cur;
         }
-        return dp[n][n];
+        // for(int i=0;i<=n;i++){
+        //     for(int j=0;j<=n;j++){
+        //         cout << dp [i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
+        return cur[n];
     }
 };
