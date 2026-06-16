6d12a0db1b9dc192f2d16f1b1d249db239052bdf
vik <theheero007@Gmail.com>
2026-06-16 20:52:13 +0530

    Time: 22 ms (80.54%) | Memory: 9.5 MB (98.18%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -15,22 +15,40 @@ public:
        reverse(t.begin(),t.end());
 
        int n = s.size();
-       vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+
+       //vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
 
        // int len = findPalin(s, t, n, n, dp);
 
-       for(int i=0;i<=n;i++) dp[i][0] = 0;
-       for(int j=0;j<=n;j++) dp[0][j] = 0;
+
+       // Tabulation
+    //    for(int i=0;i<=n;i++) dp[i][0] = 0;
+    //    for(int j=0;j<=n;j++) dp[0][j] = 0;
+
+    //    for(int i=1;i<=n;i++){
+    //         for(int j=1;j<=n;j++){
+    //             if(s[i-1] == t[j-1]){
+    //                 dp[i][j] = 1 + dp[i-1][j-1];
+    //             }
+    //             else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+    //         }
+    //    }
+    //   int len = dp[n][n];
+
+    vector<int> prev(n+1,0), curr(n+1,0);
+
+    for(int i=0;i<=n;i++) curr[0] = 0;
 
        for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
                 if(s[i-1] == t[j-1]){
-                    dp[i][j] = 1 + dp[i-1][j-1];
+                    curr[j] = 1 + prev[j-1];
                 }
-                else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                else curr[j] = max(prev[j], curr[j-1]);
             }
+            prev = curr;
        }
-       int len = dp[n][n];
+       int len = curr[n];
        cout << len;
        return n-len;
     }
