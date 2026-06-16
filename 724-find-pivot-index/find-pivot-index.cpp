79c1a8d6446256b4b4d09927910d060460d01a12
vik <theheero007@Gmail.com>
2026-06-16 12:01:49 +0530

    Time: 4 ms (15.16%) | Memory: 36.2 MB (23.65%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-    int total = accumulate(nums.begin(), nums.end(), 0);
-    int leftSum = 0;
-    for (int i = 0; i < nums.size(); ++i) {
-        if (leftSum == total - leftSum - nums[i])
-            return i;
-        leftSum += nums[i];
+        // int total = accumulate(nums.begin(), nums.end(), 0);
+        // int leftSum = 0;
+        // for (int i = 0; i < nums.size(); ++i) {
+        //     if (leftSum == total - leftSum - nums[i])
+        //         return i;
+        //     leftSum += nums[i];
+        // }
+        // return -1;
+        vector<int> prefixSum(nums.size(),0);
+        int sum = 0;
+        for(int i=0;i<nums.size();i++){
+            sum += nums[i];
+            prefixSum[i] = sum;
+        }
+        for(int i=0;i<nums.size();i++){
+            if(prefixSum[i] - nums[i] == sum - prefixSum[i]) return i;
+        }
+        return -1;
     }
-    return -1;
-}
 };
