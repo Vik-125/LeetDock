347ef519a89739cdcd4b3e680c560485077ab753
vik <theheero007@Gmail.com>
2026-06-16 21:18:20 +0530

    Time: 15 ms (32.83%) | Memory: 17.3 MB (10.88%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -18,18 +18,36 @@ public:
         vector<vector<int>> dp(l1+1,vector<int>(l2+1,-1));
         //int len = findOps(word1, word2, l1-1, l2-1, dp);
 
-        for(int i=0;i<=l1;i++) dp[i][0] = 0;
-        for(int j=0;j<=l2;j++) dp[0][j] = 0;
+
+
+        // Tabulation
+
+
+        // for(int i=0;i<=l1;i++) dp[i][0] = 0;
+        // for(int j=0;j<=l2;j++) dp[0][j] = 0;
+
+        // for(int i=1;i<=l1;i++){
+        //     for(int j=1;j<=l2;j++){
+        //         if(word1[i-1] == word2[j-1]){
+        //             dp[i][j] = 1 + dp[i-1][j-1];
+        //         }
+        //         else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+        //     }
+        // }
+        // int len = dp[l1][l2];
+        vector<int> prev(l2+1,0), curr(l2+1,0);
 
         for(int i=1;i<=l1;i++){
             for(int j=1;j<=l2;j++){
                 if(word1[i-1] == word2[j-1]){
-                    dp[i][j] = 1 + dp[i-1][j-1];
+                    curr[j] = 1 + prev[j-1];
                 }
-                else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                else curr[j] = max(prev[j],curr[j-1]);
             }
+            prev = curr;
         }
-        int len = dp[l1][l2];
+
+        int len = prev[l2];
         cout << len;
         return (l1+l2) - (2*len);
     }
