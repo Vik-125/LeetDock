babfdeb769ec9f9548ac8262d6060110817fa687
vik <theheero007@Gmail.com>
2026-06-19 22:42:05 +0530

    Time: 7 ms (34.33%) | Memory: 24.8 MB (25.43%) - LeetSync

---
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int findGain(vector<int>& prices, int i, bool buy, vector<vector<int>> &dp){
+        if(i == prices.size()) return 0;
+        if(dp[i][buy] != -1) return dp[i][buy];
+
+        if(buy){ 
+            int take = -prices[i] + findGain(prices, i+1, !buy, dp);
+            int skip = findGain(prices, i+1, buy, dp);
+            return dp[i][buy] = max(skip,take);
+        }
+        else { 
+            int sell = prices[i] + findGain(prices, i+1, !buy, dp);
+            int skip = findGain(prices, i+1, buy, dp);
+            return dp[i][buy] = max(sell,skip);
+        }
+    }
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+
+        // Memoization
+        //vector<vector<int>> dp(n,vector<int>(2,-1));
+        //return findGain(prices, 0, 1, dp);
+
+        // Tabulation
+        vector<vector<int>> dp(n+1,vector<int>(2,0));
+        dp[n][0] = dp[n][1] = 0;
+        for(int i=n-1;i>=0;i--){
+            for(int buy=0;buy<=1;buy++){
+                if(buy){ 
+                    int take = -prices[i] + dp[i+1][0];
+                    int skip = dp[i+1][1];
+                    dp[i][1] = max(skip,take);
+                }
+                else { 
+                    int sell = prices[i] + dp[i+1][1];
+                    int skip = dp[i+1][0];
+                    dp[i][0] = max(sell,skip);
+                }
+            }
+        }
+        return dp[0][1];
+    }
+};
