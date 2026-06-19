ce76304d00689424a58bb0b63e780cbe4f2530e7
vik <theheero007@Gmail.com>
2026-06-19 11:13:01 +0530

    Time: 41 ms (68.46%) | Memory: 54.6 MB (15.68%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     bool findResult(string& s, string& p, int n, int m, vector<vector<int>> &dp) {
-        if (n < 0 && m < 0) return true;
-        if (m < 0 && n >= 0) return false;
-        if (n < 0 && m >= 0){
-            while(m >= 0){
-                if(p[m] != '*') return false;
+        if (n == 0 && m == 0) return true;
+        if (m == 0 && n > 0) return false;
+        if (n == 0 && m > 0){
+            while(m > 0){
+                if(p[m-1] != '*') return false;
                 m--;
             }
             return true;
         }
         if(dp[n][m] != -1) return dp[n][m];
-        if (s[n] == p[m] || p[m] == '?') {
+        if (s[n-1] == p[m-1] || p[m-1] == '?') {
             return dp[n][m] = findResult(s, p, n - 1, m - 1, dp);
         } 
-        else if (p[m] == '*') {
-            return dp[n][m] = findResult(s, p, n, m - 1, dp) ||    findResult(s, p, n - 1, m, dp);
+        else if (p[m-1] == '*') {
+            return dp[n][m] = findResult(s, p, n, m - 1, dp) || findResult(s, p, n - 1, m, dp);
         }
         return false;
     }
     bool isMatch(string s, string p) {
         int n = s.size();
         int m = p.size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return findResult(s, p, n - 1, m - 1, dp);
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        return findResult(s, p, n, m, dp);
     }
 };
