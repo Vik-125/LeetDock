e631b42ca45414cb7e124cc787f509c6ca8f7b2d
vik <theheero007@Gmail.com>
2026-06-19 10:04:02 +0530

    Time: 2 ms (99.78%) | Memory: 8.8 MB (97.73%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -16,20 +16,32 @@ public:
         int n1 = s.size();
         int n2 = t.size();
 
-        vector<vector<int>> dp(n1+1,vector<int>(n2+1,0));
+        // vector<vector<int>> dp(n1+1,vector<int>(n2+1,0));
 
-        for(int i=0;i<=n1;i++) dp[i][0] = 1;
+        // for(int i=0;i<=n1;i++) dp[i][0] = 1;
+
+        // for(int i=1;i<=n1;i++){
+        //     for(int j=1;j<=n2;j++){
+        //         if(s[i-1] == t[j-1]){
+        //             long pick = dp[i-1][j-1];
+        //             long notPick = dp[i-1][j];
+        //             dp[i][j] = (pick + notPick);
+        //         }
+        //         else dp[i][j] = dp[i-1][j];
+        //     }
+        // }
+        vector<int> prev(n2+1,0),curr(n2+1,0);
+        prev[0] = 1;
 
         for(int i=1;i<=n1;i++){
-            for(int j=1;j<=n2;j++){
+            for(int j=n2;j>=1;j--){
                 if(s[i-1] == t[j-1]){
-                    long pick = dp[i-1][j-1];
-                    long notPick = dp[i-1][j];
-                    dp[i][j] = (pick + notPick);
+                    long pick = prev[j-1];
+                    long notPick = prev[j];
+                    prev[j] = (pick + notPick);
                 }
-                else dp[i][j] = dp[i-1][j];
             }
         }
-        return dp[n1][n2];
+        return prev[n2];
     }
 };
