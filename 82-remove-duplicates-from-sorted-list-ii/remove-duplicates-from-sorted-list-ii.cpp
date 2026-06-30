5a6d52f5d496e839d5a32bbf195ddcb0639ae392
vik <theheero007@Gmail.com>
2026-06-30 18:33:03 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (40.67%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
@@ -11,25 +11,41 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        map<int,int> hash;
-        ListNode* temp = head;
+        // map<int,int> hash;
+        // ListNode* temp = head;
     
-        while(temp){
-            hash[temp -> val]++;
-            temp = temp -> next;
-        }
+        // while(temp){
+        //     hash[temp -> val]++;
+        //     temp = temp -> next;
+        // }
 
-        ListNode* dummy = new ListNode(0);
-        ListNode* heed = dummy;
+        // ListNode* dummy = new ListNode(0);
+        // ListNode* heed = dummy;
 
-        for(auto it : hash){
-            cout << it.first;
-            if(it.second == 1){
-                ListNode* curr = new ListNode(it.first);
-                dummy -> next = curr;
-                dummy = dummy -> next;
+        // for(auto it : hash){
+        //     if(it.second == 1){
+        //         ListNode* curr = new ListNode(it.first);
+        //         dummy -> next = curr;
+        //         dummy = dummy -> next;
+        //     }
+        // }
+        // return heed -> next;
+
+        ListNode* dummy = new ListNode(0,head);
+        ListNode* prev = dummy;
+
+        while(head){
+            if(head -> next && head -> val == head -> next -> val){
+                while(head -> next && head -> val == head -> next -> val){ head = head -> next;
+                }
+                prev -> next = head -> next;
             }
+            else{ 
+                prev -> next = head;
+                prev = prev -> next;
+            }
+            head = head -> next;
         }
-        return heed -> next;
+        return dummy -> next;
     }
 };
