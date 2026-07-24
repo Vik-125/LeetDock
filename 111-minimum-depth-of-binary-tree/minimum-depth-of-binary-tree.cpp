03bc0384797e8faf65c138d3e448191f923510de
vik <theheero007@Gmail.com>
2026-07-24 14:42:36 +0530

    Time: 3 ms (54.28%) | Memory: 146.8 MB (52.13%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
@@ -6,29 +6,23 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    void check(TreeNode* root , int &mini,int height){
-        if(!root) return;
-        if(!root -> left && !root -> right)
-        {
-            mini = min(mini,height);
-            return;
-        }
-        check(root -> left,mini,height+1);
-        check(root -> right,mini,height+1);
-
-        return;
-    }
     int minDepth(TreeNode* root) {
-        if(!root) return 0;
-        int mini = INT_MAX;
-        int height = 1;
+        if (!root)
+            return 0;
+        if (!root->left && !root->right)
+            return 1;
 
-        check(root,mini,height);
-        return mini;
+        if (!root->right)
+            return 1 + minDepth(root->left);
+        if (!root->left)
+            return 1 + minDepth(root->right);
+
+        return 1 + min(minDepth(root->left), minDepth(root->right));
     }
 };
