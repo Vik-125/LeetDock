c9a194ac697adc7992aae5272888844ff3066123
vik <theheero007@Gmail.com>
2026-08-23 14:12:07 +0530

    Time: 0 ms (100.00%) | Memory: 25.5 MB (71.48%) - LeetSync

--- 530-minimum-absolute-difference-in-bst/minimum-absolute-difference-in-bst.cpp
+++ 530-minimum-absolute-difference-in-bst/minimum-absolute-difference-in-bst.cpp
@@ -12,23 +12,19 @@
 class Solution {
 public:
     int diff = INT_MAX;
-    int getMinimumDifference(TreeNode* root) {
-        vector<int> arr;
-        queue<TreeNode*> q;
-        q.push(root);
-        while(!q.empty()){
-            auto it = q.front();
-            q.pop();
-            arr.push_back(it -> val);
+    TreeNode* prev = nullptr;
+    void inorder(TreeNode* root){
+        if(!root) return;
 
-            if(it -> left) q.push(it -> left);
-            if(it -> right) q.push(it -> right);
-        }
-        for(int i=0;i<arr.size();i++){
-            for(int j=i+1;j<arr.size();j++){
-                diff = min(diff, abs(arr[i]-arr[j]));
-            }
-        }
+        inorder(root -> left);
+        if(prev) diff = min(diff,abs(root->val - prev -> val));
+
+        prev = root;
+        inorder(root -> right);
+        return;
+    }
+    int getMinimumDifference(TreeNode* root) {
+        inorder(root);
         return diff;
     }
-};
+}; 
