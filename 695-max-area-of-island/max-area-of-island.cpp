592256b742e81b55c45da8cf4919de67ab4f500a
vik <theheero007@Gmail.com>
2026-09-05 13:27:41 +0530

    Time: 0 ms (100.00%) | Memory: 28.4 MB (47.04%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.cpp
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -1,35 +1,39 @@
 class Solution {
 public:
-    int dfs(int row,int col,vector<vector<int>> &grid,int n,int m,int deltaCol[],int deltaRow[])
-    {
-        grid[row][col] = -1;
-        int area = 1;
-        for(int i=0;i<4;i++)
-        {
-            int nrow = row + deltaRow[i];
-            int ncol = col + deltaCol[i];
-            if(nrow < n && ncol < m && nrow >= 0 && ncol >= 0 && grid[nrow][ncol] == 1)
-            {   
-                area += dfs(nrow,ncol,grid,n,m,deltaCol,deltaRow);
+    void findArea(vector<vector<int>>& grid, vector<vector<bool>>& vis, int r, int c, int R, int C, int drow[], int dcol[], int &area){
+        if(vis[r][c] || grid[r][c] == 0) return;
+
+        vis[r][c] = true;
+
+        for(int i=0;i<4;i++){
+            int nr = r + drow[i];
+            int nc = c + dcol[i];
+
+            if(nr >= 0 && nr < R && nc >= 0 && nc < C && !vis[nr][nc] && grid[nr][nc] == 1){
+                area++;
+                findArea(grid, vis, nr, nc, R, C, drow, dcol, area);
             }
         }
-        return area;
+        return;
     }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int maxArea = 0;
+        int dcol[] = {0,1,0,-1};
+        int drow[] = {1,0,-1,0};
+
         int n = grid.size();
         int m = grid[0].size();
-        int deltaRow[] = {0,-1,0,1};
-        int deltaCol[] = {-1,0,1,0};
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(grid[i][j] == 1)
-                {
-                    int area = dfs(i,j,grid,n,m,deltaCol,deltaRow);
-                    maxArea = max(maxArea, area);
+        int maxArea = 0;
+
+        vector<vector<bool>> vis(n,vector<bool>(m,false));
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int area = 0;
+                if(!vis[i][j] && grid[i][j] == 1){
+                    area++;
+                    findArea(grid, vis, i, j, n, m, drow, dcol, area);
                 }
+                maxArea = max(area, maxArea);
             }
         }
         return maxArea;
