775ed616ea88d8eeb35e8fa08ccbd2c7a3f097af
vik <theheero007@Gmail.com>
2026-09-18 10:49:17 +0530

    Time: 22 ms (1.05%) | Memory: 21.4 MB (95.58%) - LeetSync

--- 112-path-sum/path-sum.cpp
+++ 112-path-sum/path-sum.cpp
@@ -13,8 +13,10 @@ class Solution {
 public:
     bool hasPathSum(TreeNode* root, int targetSum) {
         if(!root) return false;
-        if(targetSum - root -> val == 0 && !root -> left && !root -> right) return true;
+        if(!root -> left && !root -> right && targetSum - root -> val == 0) return true;
 
-        return (hasPathSum(root -> left, targetSum - root -> val) || hasPathSum(root -> right, targetSum - root -> val));
+        targetSum = targetSum - root -> val;
+        cout << targetSum << " ";
+        return (hasPathSum(root -> left, targetSum) || hasPathSum(root -> right, targetSum));
     }
 };
