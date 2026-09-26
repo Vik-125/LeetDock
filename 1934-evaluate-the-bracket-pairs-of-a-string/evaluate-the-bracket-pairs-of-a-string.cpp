c264d27287aa13787bbbd2e2d19b425471c2506a
vik <theheero007@Gmail.com>
2026-09-26 11:59:03 +0530

    Time: 134 ms (20.87%) | Memory: 144.8 MB (6.23%) - LeetSync

---
+++ 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string evaluate(string s, vector<vector<string>>& knowledge) {
+        unordered_map<string,string> hash;
+        for(auto it : knowledge){
+            string a = it[0];
+            string b = it[1];
+            hash[a] = b;
+        }
+        
+        int n = s.size();
+        string result = "";
+        int i = 0;
+
+        while(i < n){
+            if(s[i] == '('){
+                i++;
+                string temp = "";
+                while(s[i] != ')'){
+                    temp += s[i];
+                    i++;
+                }
+                i++;
+                if(hash.find(temp) != hash.end()){
+                    result += hash[temp];
+                }
+                else result += '?';
+            }
+            else{
+                result += s[i];
+                i++;
+            }
+        }
+        return result;
+    }
+};
